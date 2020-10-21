7b7a9ebfa389e3262bb4682ebd9a3fe94a4d4741
slwang613928 <865499908@qq.com>
2020-10-21 18:53:27 +0800

    Java-第一周作业
    Java-第一周作业

---
+++ Week_01/Hello.java
@@ -0,0 +1,17 @@
+package com.jk.week1;
+
+public class Hello {
+    public static void main(String[] args) {
+        int a1 = 2;
+        int a2 = 4;
+        int s1 = a1 + a2;
+        int s2 = a2 - a1;
+        int s3 = a2 * a1;
+        if (a1 == 2) {
+            int s4 = a2 % a1;
+        }
+        for (int i =0 ;i<4;i++) {
+            // System.out.println("数字=="+i);
+        }
+   }
+}
